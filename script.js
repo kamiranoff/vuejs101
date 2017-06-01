ec7df2ce98655fb0294a9028cb40338b39ed8951
Kevin Amiranoff <kamiranoff@gmail.com>
2017-06-02 00:00:13 +0200

    more vue js basics

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
 new Vue({
-  el: '#app',
+  el: '#exercise',
   data: {
-    title: 'Hello World!',
-    marvelEncyclodpediaLink: 'http://dev.kevinamiranoff.com'
+    name: 'Kevin',
+    age: 20, // yep, lets say I am 20 again.
+    imageLink: 'https://upload.wikimedia.org/wikipedia/en/c/c8/Marvelwolverine.jpg',
+    counter: 0,
+    x: 0,
+    y: 0,
+    inputValue: null,
   },
   methods: {
-    changeTitle: function(event) {
-      this.title = event.target.value;
+    multiplyAgeBy: function(factor) {
+      return this.age * factor;
     },
-    sayHello: function() {
-      return this.title;
+    generateRandomNum: function(base, top) {
+      return Math.floor((Math.random() * top) + base);
+    },
+    increment: function() {
+      this.counter++;
+    },
+    getMouseCoordinates: function(event) {
+      this.x = event.clientX;
+      this.y = event.clientY;
+    },
+    displayAlert: function(msg) {
+      alert(msg);
+    },
+    getInputValue: function(event) {
+      if (event.keyCode === 13) {
+        this.inputValue = event.target.value;
+      }
     }
   }
 });
